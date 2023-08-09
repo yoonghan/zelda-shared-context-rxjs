e29f8df2735f54e82794908c7a0e176175871774
han <walcoorperation@gmail.com>
2023-08-09 12:26:12 +0800

    Remove husky

--- declaration/authentication.d.ts
+++ declaration/authentication.d.ts
@@ -1,8 +1,8 @@
 import { BehaviorSubject } from "rxjs";
 type AuthProps = {
-    sessionToken: string | null;
-    error: string | undefined;
-    pending: boolean;
+  sessionToken: string | null;
+  error: string | undefined;
+  pending: boolean;
 };
 export declare const INTERVAL_CHECK_IN_MILISECONDS = 2500;
 export declare const auth$: BehaviorSubject<AuthProps>;
