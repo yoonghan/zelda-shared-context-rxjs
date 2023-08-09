b5712d8d0ed4902713906daef9e58e28d9174f22
han <walcoorperation@gmail.com>
2023-08-09 12:24:51 +0800

    Add typings for distribution

---
+++ declaration/authentication.d.ts
@@ -0,0 +1,11 @@
+import { BehaviorSubject } from "rxjs";
+type AuthProps = {
+    sessionToken: string | null;
+    error: string | undefined;
+    pending: boolean;
+};
+export declare const INTERVAL_CHECK_IN_MILISECONDS = 2500;
+export declare const auth$: BehaviorSubject<AuthProps>;
+export declare function login(username: string, password: string): void;
+export declare function logout(): void;
+export {};
---
+++ declaration/walcron-zelda-shared-context.d.ts
@@ -0,0 +1,2 @@
+export * as authentication from "./authentication";
+export declare const name = "shared-context";
