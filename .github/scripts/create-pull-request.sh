bf207fcd6dbd2b6388ae5d2244955b560d6bfd81
han <walcoorperation@gmail.com>
2023-08-10 01:06:21 +0800

    Update shared context

---
+++ .github/scripts/create-pull-request.sh
@@ -0,0 +1,28 @@
+BRANCH_NAME=shared-context-dist-$(date +%s)
+REPO_NAME=$1
+PROJECT_NAME=shared-context
+GH_USER_NAME=${{ vars.GH_USER_NAME }}
+GH_USER_EMAIL=${{ vars.GH_USER_EMAIL }}
+
+echo "BRANCH_NAME: $BRANCH_NAME"
+echo "REPO_NAME: $REPO_NAME"
+echo "PROJECT_NAME: $PROJECT_NAME"
+echo "GH_USER_NAME: $GH_USER_NAME"
+echo "GH_USER_EMAIL: $GH_USER_EMAIL"
+
+cd $REPO_NAME
+git branch $BRANCH_NAME
+git checkout $BRANCH_NAME
+cd ..
+rm -rf $REPO_NAME/external_modules/$PROJECT_NAME
+mkdir $REPO_NAME/external_modules/$PROJECT_NAME
+cp -rf dist/* $REPO_NAME/external_modules/$PROJECT_NAME
+echo "list distribution folder"
+ls -lrt dist/*
+cd $REPO_NAME
+git config user.name $GH_USER_NAME
+git config user.email $GH_USER_EMAIL
+git add .
+git commit -m "Generate new $PROJECT_NAME dist"
+git push origin $BRANCH_NAME
+gh pr create --base master --title "New $PROJECT_NAME distribution" --body "New declaration generated and ready to be merged to master"
