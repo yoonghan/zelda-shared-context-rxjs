60b77d4e4d978083f23a0f66d166179ef5e8f677
han <walcoorperation@gmail.com>
2023-10-21 07:43:27 +0800

    Group up test

--- src/authentication.test.ts
+++ src/authentication.test.ts
@@ -11,91 +11,95 @@ describe('authentication', () => {
     })
   })
 
-  it('should be able to login successfully, then logout', (done) => {
-    login(credentials.username, credentials.password)
+  describe('login', () => {
+    it('should be able to login successfully, then logout', (done) => {
+      login(credentials.username, credentials.password)
 
-    expect(auth$.value).toStrictEqual({
-      sessionToken: null,
-      error: undefined,
-      pending: true,
+      expect(auth$.value).toStrictEqual({
+        sessionToken: null,
+        error: undefined,
+        pending: true,
+      })
+
+      setTimeout(() => {
+        expect(auth$.value).toStrictEqual({
+          sessionToken: 'testToken',
+          error: undefined,
+          pending: false,
+        })
+        logout().then(() => {
+          expect(auth$.value).toStrictEqual({
+            pending: false,
+            error: undefined,
+            sessionToken: null,
+          })
+          done()
+        })
+      }, 200)
     })
 
-    setTimeout(() => {
+    it('should be able fail to login, and still triggers logout', (done) => {
+      login('invaliduser', 'invalidpassword')
       expect(auth$.value).toStrictEqual({
+        sessionToken: null,
+        error: undefined,
+        pending: true,
+      })
+      setTimeout(() => {
+        expect(auth$.value).toStrictEqual({
+          sessionToken: null,
+          error: 'Invalid user or password',
+          pending: false,
+        })
+        logout().then(() => {
+          expect(auth$.value).toStrictEqual({
+            pending: false,
+            error: undefined,
+            sessionToken: null,
+          })
+          done()
+        })
+      }, 200)
+    })
+
+    it("should skip double relogin relogin when it's pending", () => {
+      login('invaliduser', 'invalidpassword')
+      login('invaliduser', 'invalidpassword')
+      expect(auth$.value).toStrictEqual({
+        sessionToken: null,
+        error: undefined,
+        pending: true,
+      })
+    })
+  })
+
+  describe('create', () => {
+    it('should be able to create user successfully, then logout', async () => {
+      const response = await create(credentials.username, credentials.password)
+
+      expect(response).toStrictEqual({
+        isProfileUpdated: true,
         sessionToken: 'testToken',
         error: undefined,
         pending: false,
       })
-      logout().then(() => {
-        expect(auth$.value).toStrictEqual({
-          pending: false,
-          error: undefined,
-          sessionToken: null,
-        })
-        done()
-      })
-    }, 200)
-  })
-
-  it('should be able fail to login, and still triggers logout', (done) => {
-    login('invaliduser', 'invalidpassword')
-    expect(auth$.value).toStrictEqual({
-      sessionToken: null,
-      error: undefined,
-      pending: true,
-    })
-    setTimeout(() => {
+      await logout()
       expect(auth$.value).toStrictEqual({
+        pending: false,
+        error: undefined,
         sessionToken: null,
-        error: 'Invalid user or password',
+      })
+    })
+
+    it('should not be able to create user with profile if user fail', async () => {
+      const response = await create('failcreateuser', credentials.password)
+
+      expect(response).toStrictEqual({
+        isProfileUpdated: false,
+        sessionToken: null,
+        error: 'fail to create user.',
         pending: false,
       })
-      logout().then(() => {
-        expect(auth$.value).toStrictEqual({
-          pending: false,
-          error: undefined,
-          sessionToken: null,
-        })
-        done()
-      })
-    }, 200)
-  })
-
-  it('should be able to create user successfully, then logout', async () => {
-    const response = await create(credentials.username, credentials.password)
-
-    expect(response).toStrictEqual({
-      isProfileUpdated: true,
-      sessionToken: 'testToken',
-      error: undefined,
-      pending: false,
-    })
-    await logout()
-    expect(auth$.value).toStrictEqual({
-      pending: false,
-      error: undefined,
-      sessionToken: null,
-    })
-  })
-
-  it('should not be able to create user with profile if user fail', async () => {
-    const response = await create('failcreateuser', credentials.password)
-
-    expect(response).toStrictEqual({
-      isProfileUpdated: false,
-      sessionToken: null,
-      error: 'fail to create user.',
-      pending: false,
-    })
-  })
-
-  it("should skip double relogin relogin when it's pending", () => {
-    login('invaliduser', 'invalidpassword')
-    login('invaliduser', 'invalidpassword')
-    expect(auth$.value).toStrictEqual({
-      sessionToken: null,
-      error: undefined,
-      pending: true,
     })
   })
 })
